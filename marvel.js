caf8c260f64dfa3bad9ade19ea62ddecaca8ff93
Mya <myaetsang@gmail.com>
2018-10-21 07:50:10 -0400

    series

--- marvel.js
+++ marvel.js
@@ -67,6 +67,9 @@ function getBio(character, callback)
 	});
 }
 
+function getRandomCharacters() {
+
+}
 function getComicCover(comic, callback)
 {
 	var requestStr = 'https://gateway.marvel.com/v1/public/characters?title=' + comic +
@@ -117,7 +120,7 @@ function getSeriesByName(series, callback)
 
 	request(requestStr, {json: true}, function(error, response, body)
 	{
-		var desc = body.data.results[0].description || "No Description";
+		var desc = body.data.results[0] || "None";
 		callback(desc);
 	});
 }
