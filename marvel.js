b3489cd9fd05a086067002fdabc0f1e284dd02f3
Mya <myaetsang@gmail.com>
2018-10-20 23:02:16 -0400

    cleaned up training routes

--- marvel.js
+++ marvel.js
@@ -18,73 +18,6 @@ function getBio(character, callback)
 	});
 }
 
-function getCharacters(offset, cumulativeNames, callback)
-{
-	var promise = new Promise((resolve, reject) =>
-	{
-		console.log("promise started " + offset);
-		var requestStr = 'https://gateway.marvel.com/v1/public/characters?offset=' + offset +
-				'&limit=100' +
-				'&ts=' + ts +
-				'&apikey=' + publicKey +
-				'&hash=' + hash;
-
-		request(requestStr, {json:true}, function (error, response, body) {
-
-			count = body.data.count;
-			for (var i = 0; i < count; i++)
-			{
-				cumulativeNames.push(
-					{"canonicalForm": body.data.results[i].name,
-					"list":[]}
-				);
-			}
-
-			resolve({continue: count == 100, names: cumulativeNames})
-		});
-	});
-
-	promise.then((data) =>{
-		if (data.continue)
-			getCharacters(offset+100, data.names, callback);
-		else
-			callback(data.names);
-	});
-}
-
-function getComics(offset, cumulativeComics, callback)
-{
-	var promise = new Promise((resolve, reject) =>
-	{
-		console.log("promise started " + offset);
-		var requestStr = 'https://gateway.marvel.com/v1/public/comics?offset=' + offset +
-				'&limit=100' +
-				'&ts=' + ts +
-				'&apikey=' + publicKey +
-				'&hash=' + hash;
-
-		request(requestStr, {json:true}, function (error, response, body) {
-
-			count = body.data.count;
-			for (var i = 0; i < count; i++)
-			{
-				cumulativeComics.push(
-					{"canonicalForm": body.data.results[i].title,
-					"list":[]}
-				);
-			}
-
-			resolve({continue: count == 100, comics: cumulativeComics})
-		});
-	});
-
-	promise.then((data) =>{
-		if (data.continue)
-			getComics(offset+100, data.comics, callback);
-		else
-			callback(data.comics);
-	});
-}
 
 function getComic(callback) {
 	var requestStr = 'https://gateway.marvel.com/v1/public/comics?offset=' + 0 +
@@ -102,6 +35,4 @@ function getComic(callback) {
 
 
 exports.getBio = getBio;
-exports.getCharacters = getCharacters;
-exports.getComics = getComics;
 exports.getComic = getComic;
