894ba160eb2c2884565164defa87045590f1f99e
minersail <jkwoo1234@gmail.com>
2018-10-21 03:58:08 -0400

    added spellchecker

--- marvel.js
+++ marvel.js
@@ -1,10 +1,28 @@
 var md5 = require('md5');
 var request = require('request');
+var SpellChecker = require('simple-spellchecker');
+var Character = require('./models').Character;
 
 var publicKey = 'c15ef0d1a88a996980a054a9cc28a7ab';
 var privateKey = 'b69559945c9403fe0109082e377f0271c1d540e6';
 var ts = '1';
 var hash = md5(ts + privateKey + publicKey);
+
+function test(callback)
+{
+	Character.find({}, "name", function(err, characters)
+	{
+		var words = characters.map((char) => char.name);
+
+		SpellChecker.getDictionary("en-US", "node_modules/simple-spellchecker/dict/", function(err, result) {
+			var dictionary = result;
+
+			dictionary.setWordlist(words);
+
+			callback(dictionary.getSuggestions("Yabab", 5, 5));
+		});
+	});
+}
 
 function getBio(character, callback)
 {
@@ -27,8 +45,6 @@ function getCharacterByName(character, callback) {
 	request(requestStr, {json:true}, function (error, response, body) {
 		callback(body.data.results[0]);
 	});
-
-
 }
 
 
@@ -128,5 +144,5 @@ exports.getBio = getBio;
 exports.getComic = getComic;
 exports.getAssociatedCharacters = getAssociatedCharacters;
 exports.getCharacterByName = getCharacterByName;
-
+exports.test = test;
 
